e7ca53cef802f65f53c8a05f4101892d184cbef7
shelrobin <shelbyrobin7@gmail.com>
2016-01-19 21:01:56 -0500

    edits

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+var barWidth = 600;
+var barHeight = 100;
+
 function setup() {
 createCanvas(600,600);
 
@@ -12,26 +15,27 @@ rect(0,100,600,100)
   
 
 fill(300,200,0)
-rect(0,200,600,100)
+rect(0,200,barWidth,barHeight)
   
 
 fill(300,300,0)
-rect(0,300,600,100)
+rect(0,300,barWidth,barHeight)
   
 
 fill(100,100,0)
-rect(0,400,600,100)
+rect(0,400,barWidth,barHeight)
 
 
 fill(0,50,50)
-rect(0,500,600,100)
+rect(0,500,barWidth,barHeight)
   
-fill(0,0,0)
+fill(0)
 ellipse(300,200,60,60)
 triangle(300,220,250,350,350,350)
   
 stroke(0)
 strokeWeight(10)
+line(284,412,284,300)
 line(316,412,316,300)
 line(mouseX,mouseY,300,275)
 line(360,190,300,275)
