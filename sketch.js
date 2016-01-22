a3bc28f8c41b9f0f00a137e6b80eb46e02e1cd91
shelrobin <shelbyrobin7@gmail.com>
2016-01-22 12:20:12 -0500

    Trying to Use Defining Function
    I am trying to do a defining function for the rectangles in the
    background and it is not working.

--- sketch.js
+++ sketch.js
@@ -5,17 +5,18 @@ function setup() {
 createCanvas(600,600);
 
 }
-
+function rainbow(x,y){
+  noStroke()
+  rect(x,y,600,100) //defining a function for the rectangle
+}
 function draw() {
-background (200,50,0);
-  
-noStroke()
-fill(300,100,0)
-rect(0,100,600,100)
+
+fill(300,100,0);
+rainbow(0,100) //trying to used the defined function
   
 
 fill(300,200,0)
-rect(0,200,barWidth,barHeight)
+rect(0,200,barWidth,barHeight) //original way of making the rectangles
   
 
 fill(300,300,0)
@@ -30,13 +31,13 @@ fill(0,50,50)
 rect(0,500,barWidth,barHeight)
   
 fill(0)
-ellipse(300,200,60,60)
-triangle(300,220,250,350,350,350)
+ellipse(300,200,60,60) //head
+triangle(300,220,250,350,350,350) //dress
   
 stroke(0)
 strokeWeight(10)
-line(284,412,284,300)
-line(316,412,316,300)
-line(mouseX,mouseY,300,275)
-line(360,190,300,275)
+line(284,412,284,300) //leg
+line(316,412,316,300) //leg
+line(mouseX,mouseY,300,275) //arm 
+line(360,190,300,275) //arm
 }
